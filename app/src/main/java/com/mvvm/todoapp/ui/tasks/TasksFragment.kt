189a7a71738a8230d9d162b9107cb51d979ee48e
artem.naumov.1999 <artem.naumov.1999@bk.ru>
2021-11-19 16:46:28 +0300

    DataStore preferences managing

--- app/src/main/java/com/mvvm/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/mvvm/todoapp/ui/tasks/TasksFragment.kt
@@ -8,10 +8,14 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mvvm.todoapp.R
+import com.mvvm.todoapp.data.SortOrder
 import com.mvvm.todoapp.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
@@ -59,21 +63,26 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
                 return true
             }
         })
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.action_sort_by_name -> {
-
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
                 true
             }
             R.id.action_sort_by_date_created -> {
-
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
                 true
             }
             R.id.action_hide_completed_tasks -> {
-
                 item.isChecked = !item.isChecked
+                viewModel.onHideCompletedClick(item.isChecked)
                 true
             }
             R.id.action_delete_all_completed_tasks ->{
--- app/src/main/java/com/mvvm/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/mvvm/todoapp/ui/tasks/TasksViewModel.kt
@@ -1,23 +1,65 @@
 package com.mvvm.todoapp.ui.tasks
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.mvvm.todoapp.data.PreferencesManager
+import com.mvvm.todoapp.data.SortOrder
 import com.mvvm.todoapp.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(
-    private val taskDao: TaskDao
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
+    //val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    //val hideCompleted = MutableStateFlow(false)
+
+    /*
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ){ query,sortOrder, hideCompleted ->
+        Triple(query,sortOrder,hideCompleted)
+    }.flatMapLatest {
+        taskDao.getTasks(it.first,it.second,it.third)
+    }
+
+     */
     val searchQuery = MutableStateFlow("")
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+
+    val preferencesFlow = preferencesManager.preferencesFlow
+    private val tasksFlow = combine(
+        searchQuery,
+        preferencesFlow
+    ){ query,filterpreferences ->
+        Pair(query,filterpreferences)
+    }.flatMapLatest {(query, filterpreferences) ->
+        taskDao.getTasks(query,
+            filterpreferences.sortOrder,
+            filterpreferences.hideCompleted)
+    }
+
+    fun onSortOrderSelected(sortOrder: SortOrder){
+        viewModelScope.launch {
+            preferencesManager.updateSortOrder(sortOrder)
+        }
+    }
+
+    fun onHideCompletedClick(hideCompleted : Boolean){
+        viewModelScope.launch {
+            preferencesManager.updateHideCompleted(hideCompleted)
+        }
     }
 
     val tasks = tasksFlow.asLiveData()
 }
+
