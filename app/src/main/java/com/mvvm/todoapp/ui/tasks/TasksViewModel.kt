5523eb24fd2082f450dc91f5e15c651808bdca1a
artem.naumov.1999 <artem.naumov.1999@bk.ru>
2021-11-19 14:16:23 +0300

    Interaction between fragment and viewmodel

--- app/src/main/java/com/mvvm/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/mvvm/todoapp/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,22 @@ package com.mvvm.todoapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.mvvm.todoapp.data.TaskDao
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import javax.inject.Inject
 
-class TasksViewModel @ViewModelInject constructor(
+@HiltViewModel
+class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val searchQuery = MutableStateFlow("")
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = tasksFlow.asLiveData()
 }
